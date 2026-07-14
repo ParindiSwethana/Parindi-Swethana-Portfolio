ddf6055080dec9f98f0798050be948e36bb3f30f
Parindi <swethanaparindi@gmail.com>
2026-07-15 04:30:49 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,93 @@
+VANTA.NET({
+        el: "#intro",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x8fd5e8,
+        backgroundColor: 0x010109,
+        points: 10.00,
+        maxDistance: 20.00,
+        spacing: 20.00
+})
+
+VANTA.WAVES({
+  el: "#projects",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x010109,
+  shininess: 11.00,
+  waveHeight: 2.00,
+  waveSpeed: 1,
+  zoom: 1.29
+})
+
+VANTA.WAVES({
+  el: "#skills",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x010109,
+  shininess: 11.00,
+  waveHeight: 2.00,
+  waveSpeed: 1,
+  zoom: 1.29
+})
+
+
+
+VANTA.CELLS({
+  el: "#contact",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  color1: 0x010109,
+  color2:  0x04042c,
+  size: 4.30,
+  speed: 2.80
+})
+
+VANTA.GLOBE({
+  el: "#certificates",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x5b92f4,
+  size: 0.50,
+  backgroundColor: 0x010109
+})
+
+VANTA.DOTS({
+  el: "#about",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x8fd5e8,
+  color2: 0x8fd5e8,
+  backgroundColor: 0x010109,
+  spacing: 80.00,
+  showLines: false
+})
